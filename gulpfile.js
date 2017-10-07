4ec230f39f23fe93d364fdf38d797a7b696b5e03
Christopher Gauffin <christopher.gauffin@gmail.com>
2017-10-07 16:18:38 +0200

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,148 @@
+// generated on 2017-09-18 using generator-webapp 3.0.1
+const gulp = require('gulp');
+const gulpLoadPlugins = require('gulp-load-plugins');
+const browserSync = require('browser-sync').create();
+const del = require('del');
+const wiredep = require('wiredep').stream;
+const runSequence = require('run-sequence');
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+
+let dev = true;
+
+gulp.task('styles', () => {
+  return gulp.src('app/css/*.scss')
+    .pipe($.plumber())
+    .pipe($.if(dev, $.sourcemaps.init()))
+    .pipe($.sass.sync({
+      outputStyle: 'expanded',
+      precision: 10,
+      includePaths: ['.']
+    }).on('error', $.sass.logError))
+    .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
+    .pipe($.if(dev, $.sourcemaps.write()))
+    .pipe(gulp.dest('www/css'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('scripts', () => {
+  return gulp.src('app/js/**/*.js')
+    .pipe($.plumber())
+    .pipe($.if(dev, $.sourcemaps.init()))
+    .pipe($.babel())
+    .pipe($.if(dev, $.sourcemaps.write('.')))
+    .pipe(gulp.dest('www/js'))
+    .pipe(reload({stream: true}));
+});
+
+function lint(files) {
+  return gulp.src(files)
+    .pipe($.eslint({ fix: true }))
+    .pipe(reload({stream: true, once: true}))
+    .pipe($.eslint.format())
+    .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
+}
+
+gulp.task('lint', () => {
+  return lint('app/js/**/*.js')
+    .pipe(gulp.dest('www/js'));
+});
+
+
+gulp.task('html', ['styles', 'scripts'], () => {
+  return gulp.src('app/*.html')
+    .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
+    .pipe($.if(/\.js$/, $.uglify({compress: {drop_console: true}})))
+    .pipe($.if(/\.css$/, $.cssnano({safe: true, autoprefixer: false})))
+    .pipe($.if(/\.html$/, $.htmlmin({
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: {compress: {drop_console: true}},
+      processConditionalComments: true,
+      removeComments: true,
+      removeEmptyAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true
+    })))
+    .pipe(gulp.dest('www'));
+});
+
+gulp.task('images', () => {
+  return gulp.src('app/images/**/*')
+    .pipe($.cache($.imagemin()))
+    .pipe(gulp.dest('www/images'));
+});
+
+gulp.task('fonts', () => {
+  return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
+    .concat('app/fonts/**/*'))
+    .pipe($.if(dev, gulp.dest('.tmp/fonts'), gulp.dest('www/fonts')));
+});
+
+gulp.task('extras', () => {
+  return gulp.src([
+    'app/*',
+    '!app/*.html'
+  ], {
+    dot: true
+  }).pipe(gulp.dest('www'));
+});
+
+gulp.task('clean', del.bind(null, ['.tmp', 'www']));
+
+gulp.task('serve', () => {
+  runSequence(['wiredep'], ['styles', 'scripts', 'fonts'], () => {
+    /*browserSync.init({
+      notify: false,
+      port: 9000,
+      server: {
+        baseDir: ['.tmp', 'app'],
+        routes: {
+          '/bower_components': 'bower_components'
+        }
+      }
+    });*/
+
+    // gulp.watch([
+    //   'app/*.html',
+    //   'app/images/**/*',
+    //   '.tmp/fonts/**/*'
+    // ]).on('change', reload);
+
+    gulp.watch('app/css/**/*.scss', ['styles']);
+    gulp.watch('app/js/**/*.js', ['scripts']);
+    gulp.watch('app/fonts/**/*', ['fonts']);
+    gulp.watch('app/*.html', ['html']);
+    gulp.watch('bower.json', ['wiredep', 'fonts']);
+  });
+});
+
+
+// inject bower components
+gulp.task('wiredep', () => {
+  gulp.src('app/css/*.scss')
+    .pipe($.filter(file => file.stat && file.stat.size))
+    .pipe(wiredep({
+      ignorePath: /^(\.\.\/)+/
+    }))
+    .pipe(gulp.dest('app/css'));
+
+  gulp.src('app/*.html')
+    .pipe(wiredep({
+      exclude: ['bootstrap'],
+      ignorePath: /^(\.\.\/)*\.\./
+    }))
+    .pipe(gulp.dest('app'));
+});
+
+gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
+  return gulp.src('www/**/*').pipe($.size({title: 'build', gzip: true}));
+});
+
+gulp.task('default', () => {
+  return new Promise(resolve => {
+    dev = false;
+    runSequence(['clean', 'wiredep'], 'build', resolve);
+  });
+});
---
+++ www/js/main.js
@@ -0,0 +1,44 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+    document.querySelector("#pic").addEventListener("click", takePic);
+    document.querySelector("#vibrate").addEventListener("click", vibrate);
+    document.querySelector("#alert").addEventListener("click", alert);
+
+    window.addEventListener("batterystatus", onBatteryStatus, false);
+    console.log(device.cordova);
+    console.log(navigator.globalization);
+    StatusBar.show();
+}
+
+function onBatteryStatus(status) {
+    console.log("Level: " + status.level + " isPlugged: " + status.isPlugged);
+}
+
+function takePic() {
+    navigator.camera.getPicture(function () {
+        console.log("success");
+    }, function () {
+        console.log("fail");
+    }, {
+        quality: 100
+    });
+}
+
+function vibrate() {
+    navigator.vibrate(3000);
+}
+
+function alert() {
+    //console.log("alert");
+    navigator.notification.alert('You are the winner!', // message
+    alertCallback, // callback
+    'Game Over', // title
+    'Done' // buttonName
+    );
+}
+
+function alertCallback() {
+    console.log("callback");
+}
+//# sourceMappingURL=main.js.map
