ea1db1c2911bcf5f4eaf8596468e103608202e6c
Christopher Gauffin <christopher.gauffin@gmail.com>
2017-10-08 14:03:04 +0200

    piccy

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,29 @@
-document.addEventListener("deviceready", onDeviceReady, false);
+
+
+$(document).ready(function(){
+  document.addEventListener("deviceready", onDeviceReady, false);
+});
+
+function supports_html5_storage() {
+  try {
+    return 'localStorage' in window && window['localStorage'] !== null;
+  } catch (e) {
+    return false;
+  }
+}
+function supports_json() {
+  try {
+    return 'JSON' in window && window['JSON'] !== null;
+  } catch (e) {
+    return false;
+  }
+}
+
 
 function onDeviceReady() {
-    document.querySelector("#pic").addEventListener("click", takePic);
-    document.querySelector("#vibrate").addEventListener("click", vibrate);
-    document.querySelector("#alert").addEventListener("click", alert);
+    $("#pic").click(takePic);
+    $("#vibrate").click(vibrate);
+    $("#alert").click(alert);
 
     window.addEventListener("batterystatus", onBatteryStatus, false);
     console.log(device.cordova);
@@ -12,18 +32,23 @@ function onDeviceReady() {
 }
 
 function onBatteryStatus(status) {
-    console.log("Level: " + status.level + " isPlugged: " + status.isPlugged);
+    $("#battery").html("Level: " + status.level + " isPlugged: " + status.isPlugged);
 }
 
 function takePic() {
     navigator.camera.getPicture(
-    function() {
+    function(imageData) {
         console.log("success");
+        changeImage(imageData);
     }, function(){
         console.log("fail");
     }, {
         quality: 100
     });
+}
+
+function changeImage(imageData){
+  $("#mainImg").attr("src", "data:image/jpeg;base64," + imageData);
 }
 
 function vibrate() {
@@ -38,6 +63,44 @@ function alert() {
         'Game Over',            // title
         'Done'                  // buttonName
     );
+}
+//Adding to storage
+function addToStorage(id,data){
+    if (!hasInStorage(id)) {
+        var storage = getStorage();
+        storage[id] = data;
+        saveStorage(data);
+    }
+}
+
+//loading from storage
+function getStorage(){
+    var current = localStorage["main"];
+    var storage = {};
+    if(typeof current != "undefined") storage=window.JSON.parse(current);
+    return storage;
+}
+
+//Checking storage
+function hasInStorage(id){
+    return (id in getStorage());
+}
+
+//Adding to storage
+function removeFromStorage(id,data){
+    if (hasInStorage(id)) {
+        var storage = getStorage();
+        delete storage[id];
+        console.log('removed '+id);
+        saveStorage(storage);
+    }
+}
+
+//save storage
+function saveStorage(data){
+    console.log("To store...");
+    console.dir(storage);
+    localStorage["main"] = window.JSON.stringify(storage);
 }
 
 function alertCallback() {
--- platforms/browser/www/js/main.js
+++ platforms/browser/www/js/main.js
@@ -1,9 +1,28 @@
-document.addEventListener("deviceready", onDeviceReady, false);
+
+
+$(document).ready(function () {
+    document.addEventListener("deviceready", onDeviceReady, false);
+});
+
+function supports_html5_storage() {
+    try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+        return false;
+    }
+}
+function supports_json() {
+    try {
+        return 'JSON' in window && window['JSON'] !== null;
+    } catch (e) {
+        return false;
+    }
+}
 
 function onDeviceReady() {
-    document.querySelector("#pic").addEventListener("click", takePic);
-    document.querySelector("#vibrate").addEventListener("click", vibrate);
-    document.querySelector("#alert").addEventListener("click", alert);
+    $("#pic").click(takePic);
+    $("#vibrate").click(vibrate);
+    $("#alert").click(alert);
 
     window.addEventListener("batterystatus", onBatteryStatus, false);
     console.log(device.cordova);
@@ -12,17 +31,22 @@ function onDeviceReady() {
 }
 
 function onBatteryStatus(status) {
-    console.log("Level: " + status.level + " isPlugged: " + status.isPlugged);
+    $("#battery").html("Level: " + status.level + " isPlugged: " + status.isPlugged);
 }
 
 function takePic() {
-    navigator.camera.getPicture(function () {
+    navigator.camera.getPicture(function (imageData) {
         console.log("success");
+        changeImage(imageData);
     }, function () {
         console.log("fail");
     }, {
         quality: 100
     });
+}
+
+function changeImage(imageData) {
+    $("#mainImg").attr("src", "data:image/jpeg;base64," + imageData);
 }
 
 function vibrate() {
@@ -36,6 +60,44 @@ function alert() {
     'Game Over', // title
     'Done' // buttonName
     );
+}
+//Adding to storage
+function addToStorage(id, data) {
+    if (!hasInStorage(id)) {
+        var storage = getStorage();
+        storage[id] = data;
+        saveStorage(data);
+    }
+}
+
+//loading from storage
+function getStorage() {
+    var current = localStorage["main"];
+    var storage = {};
+    if (typeof current != "undefined") storage = window.JSON.parse(current);
+    return storage;
+}
+
+//Checking storage
+function hasInStorage(id) {
+    return id in getStorage();
+}
+
+//Adding to storage
+function removeFromStorage(id, data) {
+    if (hasInStorage(id)) {
+        var storage = getStorage();
+        delete storage[id];
+        console.log('removed ' + id);
+        saveStorage(storage);
+    }
+}
+
+//save storage
+function saveStorage(data) {
+    console.log("To store...");
+    console.dir(storage);
+    localStorage["main"] = window.JSON.stringify(storage);
 }
 
 function alertCallback() {
